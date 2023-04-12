4f976f34c7c2f9df33d8c5f2c08c50df763ecf4a
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-12 10:57:15 +0530

    [sonamahapatra] Checking for option

--- SnakeLadderProgram/SnakeLadder.cs
+++ SnakeLadderProgram/SnakeLadder.cs
@@ -11,11 +11,26 @@ namespace SnakeLadderProgram
         internal int PlayerPossition = 0;
         public void Game()
         {
-            
             Random random = new Random();
             int DieRoll = random.Next(1, 7);
             PlayerPossition = DieRoll;
+            int Option = random.Next(0, 3);
+            switch (Option)
+            {
+                case 1:
+                    Console.WriteLine("No Play : " + PlayerPossition);
+                    break;
+                case 2:
+                    PlayerPossition += DieRoll;
+                    Console.WriteLine("lader : " + PlayerPossition);
+                    break;
+                case 3:
+                    PlayerPossition -= DieRoll;
+                    Console.WriteLine("Snake : " + PlayerPossition);
+                    break;
+            }
             Console.WriteLine("Player Position is :" + PlayerPossition);
+            Console.ReadLine();
         }
     }
 }
